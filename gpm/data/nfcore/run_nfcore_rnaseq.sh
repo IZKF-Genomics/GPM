245ee7946102ca22a27d0020c977bf2398cd5ae9
Joseph Kuo <chao-chung.kuo@ukaachen.de>
2022-01-21 12:59:27 +0100

    update

--- gpm/data/nfcore/run_nfcore_rnaseq.sh
+++ gpm/data/nfcore/run_nfcore_rnaseq.sh
@@ -13,8 +13,13 @@ nextflow run nf-core/rnaseq -r 3.5 -profile docker \
      --pseudo_aligner salmon \
      --input samplesheet.csv \
      --genome gencode_hg38 --gencode --featurecounts_group_type gene_type \ 
-     --multiqc_title GPM_TITLE_NAME
+     --multiqc_title GPM_TITLE_NAME \
+     --rseqc_modules 'bam_stat,inner_distance,infer_experiment,junction_annotation,junction_saturation,read_distribution,read_duplication'
+
 
 # Options for --genome:
 # gencode_hg38, gencode_mm10, hg38, mm10
 # Useful options: --removeRiboRNA
+
+# QuantSeq 3’ mRNA-Seq Library Prep Kits!
+# --clip_r1 12
