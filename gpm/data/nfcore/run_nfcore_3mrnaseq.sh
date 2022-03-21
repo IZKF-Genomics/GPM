ca3c0a4b241182d36655f4f5e2696c9d1a73b507
Chao-Chung Kuo <chao-chung.kuo@rwth-aachen.de>
2022-03-21 15:05:01 +0100

    modified:   gpm/data/nfcore/run_nfcore_3mrnaseq.sh

--- gpm/data/nfcore/run_nfcore_3mrnaseq.sh
+++ gpm/data/nfcore/run_nfcore_3mrnaseq.sh
@@ -10,13 +10,15 @@
 # }
 
 nextflow run nf-core/rnaseq -r 3.5 -profile docker \
-     --pseudo_aligner salmon \
      --input samplesheet.csv --clip_r1 12 \
      --genome gencode_hg38 --gencode --featurecounts_group_type gene_type \ 
      --multiqc_title GPM_TITLE_NAME \
-     --rseqc_modules 'bam_stat,inner_distance,infer_experiment,junction_annotation,junction_saturation,read_distribution,read_duplication'
+     --rseqc_modules 'bam_stat,inner_distance,infer_experiment,junction_annotation,junction_saturation,read_distribution,read_duplication' \
+     --additional_fasta /data/genomes/spikein/ERCC_ExFold_RNA_SpikeIn_Mixes/ERCC92.fa --star_index false --save_reference 
 
 
 # Options for --genome:
 # gencode_hg38, gencode_mm10, hg38, mm10
-# Useful options: --removeRiboRNA
+# Useful options: 
+# --removeRiboRNA
+# 
