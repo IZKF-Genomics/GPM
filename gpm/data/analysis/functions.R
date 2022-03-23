4c70b2d9a05724706ee7ae7e284a15f6e7ec39f8
Chao-Chung Kuo <chao-chung.kuo@rwth-aachen.de>
2022-03-23 12:59:24 +0100

    rnaseq RMD update

--- gpm/data/analysis/functions.R
+++ gpm/data/analysis/functions.R
@@ -1,3 +1,21 @@
+organism2method_des <- function(organism) {
+  if (organism == "hsapiens") {
+    method_des <- "human genome (GRCh38.p13)"
+  } else if (organism == "mmusculus") {
+    method_des <- "mouse genome (GRCm39)"
+  }
+  return(method_des)
+}
+
+spikein_ERCC2method <- function(spikein_ERCC) {
+  if (spikein_ERCC==TRUE) {
+    spikein_method <- "Reads were mapped to a composite genome made by concatenating the reference genome and 92 ERCC ExFold RNA Spike-In Mixes sequences (ThermoFisher)." 
+    } else {
+    spikein_method <- ""
+    }
+  return(spikein_method)
+}
+
 PCA_all_samples <- function(scaled_ct, colors) {
   t <- t(scaled_ct[, c(-1)])
   prin_comp <- prcomp(t, rank. = 2)
