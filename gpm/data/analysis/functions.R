73233d42a75b7974532cc1d076c50dfe1270f546
Chao-Chung Kuo <chao-chung.kuo@rwth-aachen.de>
2022-05-18 13:24:25 +0200

    update miRNAseq

--- gpm/data/analysis/functions.R
+++ gpm/data/analysis/functions.R
@@ -218,3 +218,161 @@ table_sig_genes <- function(res_sig) {
              class = c('compact cell-border stripe hover') ,
              rownames = FALSE) %>% formatRound(c(-1, -2), 2)
 }
+
+###########################################################
+## miRNAseq
+###########################################################
+
+miRNAseq_deseq2 <- function(FILE_counts_mature, FILE_counts_hairpin, samples2) {
+  df_counts1 <- t(read.csv(FILE_counts_mature, row.names = 1, header=T, sep=","))[, samples2$sample]
+  df_counts2 <- t(read.csv(FILE_counts_hairpin, row.names = 1, header=T, sep=","))[, samples2$sample]
+  df_counts <- df_counts1
+  # df_counts <- rbind(df_counts1, df_counts2)
+  dds <- DESeqDataSetFromMatrix(countData = df_counts, colData = samples2, design = ~group)
+  # ERCC normalization #####################
+  # if (spikein==TRUE) {
+  #   dds <- estimateSizeFactors_ERCC(dds)
+  # }
+  ##########################################
+  dds <- DESeq(dds)
+  res <- as.data.frame(results(dds))
+  res$sig <- "Non-sig."
+  res$sig[res$padj < CUTOFF_ADJP] <- "Sig."
+  norm_counts <- counts(dds, normalized=TRUE)
+  res_combined <- merge(res, norm_counts, by.x= "row.names", by.y="row.names", all.y=T)
+  colnames(res_combined)[1] <- "gene_name"
+  res_sig <- res[res$padj < CUTOFF_ADJP,]
+  res_sig <- res_sig[complete.cases(res_sig), ]
+  output <- list(res=res,
+                 norm_counts=norm_counts,
+                 res_combined=res_combined,
+                 res_sig=res_sig)
+  return(output)
+}
+
+miRNAseq_PCA_plotly <- function(normalized_counts, samples) {
+  t <- t(normalized_counts)
+  prin_comp <- prcomp(t, rank. = 2)
+  components <- prin_comp[["x"]]
+  components <- data.frame(components)
+  components <- cbind(components, rownames(t))
+  labels <- samples$sample
+  labels_group <- samples$group
+  fig <- plot_ly(components, x = ~PC1, y = ~PC2, color = labels_group, text = labels,
+                 width = Fig_width, height = Fig_height,
+                 type = 'scatter', mode = 'markers')
+  fig
+}
+
+miRNAseq_maplot_plotly <- function(res_combined) {
+  pal <- c("red", "royalblue")
+  pal <- setNames(pal, c("Sig.", "Non-sig."))
+  fig <- plot_ly(x = log2(res_combined$baseMean),
+                 y = res_combined$log2FoldChange,
+                 text = res_combined$gene_name,
+                 hoverinfo = 'text',
+                 type = 'scatter', mode = 'markers',
+                 marker = list(opacity = 0.2),
+                 color = res_combined$sig, colors = pal,
+                 showlegend = T)  %>%
+    layout(
+      title = "MA plot",
+      xaxis = list(title = "Mean Expression (log2)"),
+      yaxis = list(title = "Fold Change (log2)")
+    )
+  fig
+}
+
+miRNAseq_heatmap_plotly <- function(res_combined) {
+  res_reorder <- res_combined[order(res_combined$padj, decreasing = FALSE), ]
+  res_reorder <- res_reorder[1:500,]
+  heatmap_t <- log10(res_reorder[,9:(dim(res_reorder)[2])]+1)
+  rownames(heatmap_t) <- c()
+  # heatmaply(heatmap_t, main = "Heatmap of miRNAs ranked by adj. p-value",
+  #           method = "plotly")
+  heatmaply(heatmap_t, main = "Heatmap of top 500 miRNAs ranked by adj. p-value",
+            method = "plotly", #labRow=res_reorder$gene_name,
+            xlab = "Samples", ylab = "miRNA", width = Fig_width, height = Fig_height+200,
+            showticklabels = c(TRUE, FALSE), show_dendrogram = c(FALSE, TRUE),
+            key.title = "Scaled\nexpression\nin log10 scale",
+            label_names = c("gene_name", "sample", "Expression"),
+            k_col = 3)
+}
+
+miRNAseq_PCA_ggplot2 <- function(res_miRNA, samples2){
+  t <- t(res_miRNA$norm_counts)
+  prin_comp <- prcomp(t, rank. = 2)
+  components <- prin_comp[["x"]]
+  components <- data.frame(components)
+  components <- cbind(components, rownames(t))
+  labels <- samples2$sample
+  labels_group <- samples2$group
+
+  fig <- ggplot(components, aes(PC1, PC2, color=labels_group)) +
+        geom_point(size=3) + theme_bw() + scale_color_brewer(palette = "Set1") +
+        labs(color = "Groups") + ggtitle("PCA") +
+        theme(plot.title = element_text(hjust = 0.5))
+  fig
+}
+
+miRNAseq_volcano_ggplot2 <- function(res_miRNA){
+  pal <- c("red", "royalblue")
+  pal <- setNames(pal, c("Sig.", "Non-sig."))
+  xmax <- max(abs(res_miRNA$res$log2FoldChange)) * 1.1
+  ymax <- max(-log10(res_miRNA$res$padj)[is.finite(-log10(res_miRNA$res$padj))]) * 1.1
+  fig <- ggplot(res_miRNA$res, aes(log2FoldChange, -log10(padj), color=sig)) +
+        geom_point(size=1, alpha=0.2) + theme_bw() + scale_color_manual(values=pal) +
+        labs(color = "Groups") + ggtitle("Volcano plot") +
+        xlab("Fold Change (log2)") + ylab("adjusted p-value (-log10)") +
+        theme(plot.title = element_text(hjust = 0.5)) +
+        xlim(-xmax, xmax) + ylim(0, ymax)
+  fig
+}
+
+miRNAseq_maplot_ggplot2 <- function(res_miRNA){
+  pal <- c("red", "royalblue")
+  pal <- setNames(pal, c("Sig.", "Non-sig."))
+  ymax <- max(abs(res_miRNA$res$log2FoldChange)) * 1.01
+
+  fig <- ggplot(res_miRNA$res, aes(log2(baseMean), log2FoldChange, color=sig)) +
+        geom_point(size=1, alpha=0.2) + theme_bw() + scale_color_manual(values=pal) +
+        labs(color = "Groups") + ggtitle("MA plot") +
+        xlab("Expresion Mean (log2)") + ylab("Fold Change (log2)") +
+        theme(plot.title = element_text(hjust = 0.5)) +
+        ylim(-ymax, ymax)
+  fig
+}
+
+miRNAseq_heatmap_ggplot2 <- function(res_miRNA){
+  margin_spacer <- function(x) {
+    # where x is the column in your dataset
+    left_length <- nchar(levels(factor(x)))[1]
+    if (left_length > 8) {
+      return((left_length - 8) * 4)
+    }
+    else
+      return(0)
+  }
+
+  res_reorder <- res_miRNA$res_combined[order(res_miRNA$res_combined$padj, decreasing = FALSE), ]
+  res_reorder <- res_reorder[1:500,]
+  samples_names <- colnames(res_reorder)[9:(dim(res_reorder)[2])]
+  heatmap_t <- scale(log10(res_reorder[,9:(dim(res_reorder)[2])]+1))
+  ord <- hclust( dist(heatmap_t, method = "euclidean"), method = "ward.D" )$order
+
+  heatmap_t <- cbind(res_reorder$gene_name, as.data.frame(heatmap_t))
+  colnames(heatmap_t)[1] <- "gene_name"
+  heatmap_t <- pivot_longer(heatmap_t, cols=2:(dim(heatmap_t)[2]), names_to="sample", values_to="Expression")
+  heatmap_t$gene_name <- factor( heatmap_t$gene_name, levels = res_reorder$gene_name[ord])
+  heatmap_t$sample <- factor( heatmap_t$sample, levels = samples_names)
+
+  fig <- ggplot(heatmap_t, aes(sample, gene_name, fill=Expression)) +
+        geom_tile() + ggtitle("Heatmap of top 500 genes ranked by padj") +
+        ylab("Genes") + xlab("Samples") + scale_fill_viridis() +
+        theme(plot.title = element_text(hjust = 0.5),
+              axis.ticks.y = element_blank(),
+              axis.text.y = element_blank(),
+              axis.text.x = element_text(angle = 45, vjust = 1, hjust=1),
+              plot.margin = margin(l = 0 + margin_spacer(heatmap_t$sample)))
+  fig
+}
