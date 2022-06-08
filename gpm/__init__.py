d315eea1a128f0371d981b9aa1c483508afda050
Chao-Chung Kuo <chao-chung.kuo@rwth-aachen.de>
2022-06-08 22:39:29 +0200

    implement GO analyses

--- gpm/__init__.py
+++ gpm/__init__.py
@@ -2,7 +2,7 @@
 version_info = (0,2,4)
 version = ".".join([str(c) for c in version_info])
 
-APPLICATIONS = ["RNAseq", "totalRNAseq", "mRNAseq", "3mRNAseq",
+APPLICATIONS = ["RNAseq", "tRNAseq", "mRNAseq", "3mRNAseq",
                 "ChIPseq", "ATACseq", "Ampseq",
                 "scRNAseq", "miRNAseq",
                 "BWGS", "WES"]
