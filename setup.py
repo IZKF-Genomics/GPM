1bdaae064c23de24fd9101a96b65f280a5153b27
Joseph Kuo <chao-chung.kuo@ukaachen.de>
2022-09-05 09:58:53 +0200

    update setup.py for enviroment variable

--- setup.py
+++ setup.py
@@ -3,31 +3,6 @@ from gpm import version
 from os import path, getenv, makedirs, listdir, environ
 import shutil
 
-setup(
-    name="gpm",
-    version=version,
-    description = "Genomic Project Manager for automatizing the bioinformatic workflow in IZKF Genomic Facility.",
-    long_description = "This command line tool is designed to simplifiy and automatize the bioinformatic workflow in IZKF Genomic Facility.",
-    platforms = ["Linux"],
-    author="Chao-Chung Kuo",
-    author_email="chao-chung.kuo@rwth-aachen.de",
-    url="https://github.com/IZKF-Genomics/gpm",
-    license = "MIT",
-    packages = find_packages(),
-    # packages=['mypkg'],
-    package_dir = {'gpm': 'gpm'},
-    package_data = {'gpm': ['data/*', 'data/bcl2fastq/*', 'data/nfcore/*', 
-                            'data/export/*', 'data/analysis/*', 'configs/*']},
-    py_modules = ['gpm'],
-    install_requires=[
-        'Click',
-    ],
-    entry_points='''
-        [console_scripts]
-        gpm=gpm.main:main
-    ''',
-)
-
 ###################################################################################################
 # Creating Data Path
 ###################################################################################################
@@ -54,3 +29,33 @@ for file in listdir(config_dir):
     #     for line in f1.readlines():
     #         print("# "+line, file=userconfig, end="")
     # userconfig.close()
+
+###################################################################################################
+# Setup function
+###################################################################################################
+
+setup(
+    name="gpm",
+    version=version,
+    description = "Genomic Project Manager for automatizing the bioinformatic workflow in IZKF Genomic Facility.",
+    long_description = "This command line tool is designed to simplifiy and automatize the bioinformatic workflow in IZKF Genomic Facility.",
+    platforms = ["Linux"],
+    author="Chao-Chung Kuo",
+    author_email="chao-chung.kuo@rwth-aachen.de",
+    url="https://github.com/IZKF-Genomics/gpm",
+    license = "MIT",
+    packages = find_packages(),
+    # packages=['mypkg'],
+    package_dir = {'gpm': 'gpm'},
+    package_data = {'gpm': ['data/*', 'data/bcl2fastq/*', 'data/nfcore/*', 
+                            'data/export/*', 'data/analysis/*', 'configs/*']},
+    py_modules = ['gpm'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        gpm=gpm.main:main
+    ''',
+)
+
