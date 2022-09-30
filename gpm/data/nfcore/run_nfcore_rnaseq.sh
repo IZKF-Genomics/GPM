e4c9e624d1527efcf2e696b2ce94201db3e6ae2e
Joseph Kuo <chao-chung.kuo@ukaachen.de>
2022-09-30 09:14:54 +0200

    fix patterns with escape sign

--- gpm/data/nfcore/run_nfcore_rnaseq.sh
+++ gpm/data/nfcore/run_nfcore_rnaseq.sh
@@ -1,14 +1,14 @@
 ################## test script ##################
-# nextflow run nf-core/rnaseq -profile test,docker
+# nextflow run nf-core/rnaseq -r 3.8.1 -profile test,docker --outdir results
 
 ################## GPM samplesheet #####################
 # gpm samplesheet -st 'reverse' samplesheet.csv FASTQ_DIR
 
-nextflow run nf-core/rnaseq -r 3.5 -profile docker \
-     --input samplesheet.csv \
-     --genome gencode_hg38 --gencode --featurecounts_group_type gene_type \
-     --rseqc_modules 'bam_stat,inner_distance,infer_experiment,junction_annotation,junction_saturation,read_distribution,read_duplication' \
-     --additional_fasta /data/genomes/spikein/ERCC_ExFold_RNA_SpikeIn_Mixes/ERCC92.fa --star_index false --save_reference 
+nextflow run nf-core/rnaseq -r 3.8.1 -profile docker \
+     --input samplesheet.csv --outdir results \
+     --genome gencode_hg38 --gencode --featurecounts_group_type gene_type --star_index false --save_reference \
+     # --rseqc_modules 'bam_stat,inner_distance,infer_experiment,junction_annotation,junction_saturation,read_distribution,read_duplication' \
+     # --additional_fasta /data/genomes/spikein/ERCC_ExFold_RNA_SpikeIn_Mixes/ERCC92.fa  
 
 
 # Options for --genome:
