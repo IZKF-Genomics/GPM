1eb5e45896c80dc3c32c6bd41dcce5c56c32d681
Joseph Kuo <chao-chung.kuo@ukaachen.de>
2022-10-18 14:38:31 +0200

    fix RNAseq Rmd report

--- gpm/data/nfcore/run_nfcore_3mrnaseq.sh
+++ gpm/data/nfcore/run_nfcore_3mrnaseq.sh
@@ -4,10 +4,10 @@
 ################## GPM samplesheet #####################
 # gpm samplesheet -st 'forward' samplesheet.csv FASTQ_DIR
 
-nextflow run nf-core/rnaseq -r 3.5 -profile docker \
-     --input samplesheet.csv --clip_r1 12 \
+nextflow run nf-core/rnaseq -r 3.8.1 -profile docker \
+     --input samplesheet.csv --clip_r1 12 --outdir results \
      --genome gencode_hg38 --gencode --featurecounts_group_type gene_type \
-     --rseqc_modules 'bam_stat,inner_distance,infer_experiment,junction_annotation,junction_saturation,read_distribution,read_duplication' \
+     # --rseqc_modules 'bam_stat,inner_distance,infer_experiment,junction_annotation,junction_saturation,read_distribution,read_duplication' \
      --additional_fasta /data/genomes/spikein/ERCC_ExFold_RNA_SpikeIn_Mixes/ERCC92.fa --star_index false --save_reference 
 
 
