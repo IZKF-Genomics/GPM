17a565e71a33fba77f704e07a7222b17209fe30c
Joseph Kuo <chao-chung.kuo@rwth-aachen.de>
2023-03-02 14:07:35 +0100

    update scVDJseq

--- gpm/data/cellranger/run_cellranger_scVDJ.sh
+++ gpm/data/cellranger/run_cellranger_scVDJ.sh
@@ -5,26 +5,12 @@
 # otherwise scource shared enviroment from /data/shared_env/shared_paths.sh
 # this script can be run directly from shell
 
-# set directory
-Multi_Output="./multi_out"
+Local_Cores=30
 
-mkdir -p $Multi_Output
+Sample_ID="sample_1"
+# Sample_ID="sample_1 sample_2"
 
-#sample 132
-
-Conf="/data/projects/220504_Simons_Pabst_MolMedizin_scVDJseq/analysis/multi_config_220504.csv"
-sampleid=132
-cd $outDir
-cellranger multi --id=$sampleid \
---csv=$Conf \
---localcores=8 \
---localmem=64
-
-#sample 133
-Conf="/data/projects/220504_Simons_Pabst_MolMedizin_scVDJseq/analysis/multi_config_220504_1.csv"
-sampleid=133
-cd $outDir
-cellranger multi --id=$sampleid \
---csv=$Conf \
---localcores=8 \
---localmem=64
+for sample in ${Sample_ID}; do
+  Config_File="./multi_config_${sample}.csv"
+  cellranger multi --id=$sample --csv=$Config_File --localcores=$Local_Cores
+done
