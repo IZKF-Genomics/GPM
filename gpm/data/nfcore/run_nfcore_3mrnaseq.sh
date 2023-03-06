49ff268749bfb7cafd16a35a54c87b770935f7c5
Joseph Kuo <chao-chung.kuo@ukaachen.de>
2023-03-06 10:34:29 +0100

    update 3mRNAseq script

--- gpm/data/nfcore/run_nfcore_3mrnaseq.sh
+++ gpm/data/nfcore/run_nfcore_3mrnaseq.sh
@@ -6,13 +6,15 @@
 
 nextflow run nf-core/rnaseq -r 3.8.1 -profile docker \
      --input samplesheet.csv --clip_r1 12 --outdir results \
-     --genome gencode_hg38 --gencode --featurecounts_group_type gene_type \
-     # --rseqc_modules 'bam_stat,inner_distance,infer_experiment,junction_annotation,junction_saturation,read_distribution,read_duplication' \
-     --additional_fasta /data/genomes/spikein/ERCC_ExFold_RNA_SpikeIn_Mixes/ERCC92.fa --star_index false --save_reference 
+     --genome gencode_GRCh38 \
+     # --genome gencode_GRCm39 \
+     --gencode --featurecounts_group_type gene_type \
+     --star_index false --save_reference \
+     #--additional_fasta /data/genomes/spikein/ERCC_ExFold_RNA_SpikeIn_Mixes/ERCC92.fa  
 
 
 # Options for --genome:
-# gencode_hg38, gencode_mm10, hg38, mm10
+# gencode_GRCm39, gencode_mm10, hg38, mm10
 # Useful options: 
 # --removeRiboRNA
 # 
