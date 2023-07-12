ee0e8cc7b8a26344bc171e0a8e0978540a1b76d7
chaochungkuo <chao-chung.kuo@rwth-aachen.de>
2023-07-12 14:19:25 +0800

    split test scripts

--- test/test_scripts.sh
+++ test/test_scripts.sh
@@ -1,9 +1,3 @@
-# Configure the test folder and files
-rm -r FASTQ* 220101*
-
-# Demultiplexing
-gpm demultiplex --raw BCLPath --output FASTQ
-gpm demultiplex --raw BCLPath --output FASTQsc -sc True
 
 # Initiating a project
 # APP="RNAseq"
