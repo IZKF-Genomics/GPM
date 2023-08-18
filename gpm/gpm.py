a6eb2ccc1d1d4ddd32c3897a296756f9600aba4c
Ilya-Fradlin <ilyafr97@gmail.com>
2023-08-18 11:09:31 +0200

    Search config file across  fastq structure

--- gpm/gpm.py
+++ gpm/gpm.py
@@ -166,6 +166,19 @@ class GPM():
         """Adding the bcl path to the project's config file from the fastq
         config file"""
         # Getting the path from the fastq config file
+        current_dir = self.fastq
+        while True:
+            demultiplexing_config_path = os.path.join(current_dir, "config.ini")
+            if os.path.isfile(demultiplexing_config_path):
+                break
+            parent_dir = os.path.dirname(current_dir)
+            if parent_dir == current_dir:
+                # Reached the root directory without finding the config file
+                click.echo("***** config.ini file doesn't exist in the fastq folder or it's parent directories.\
+                            unable to add bcl path to the config file")
+                sys.exit()
+            current_dir = parent_dir
+
         demultiplexing_config_path = os.path.join(self.fastq, "config.ini")
         if not os.path.isfile(demultiplexing_config_path):
             click.echo("***** config.ini file doesn't exist in the *fastq "
