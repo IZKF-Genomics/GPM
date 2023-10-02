b34862f73bd9ed6cd43e1d2ae41b3fc9e2d98045
Joseph Kuo <chao-chung.kuo@rwth-aachen.de>
2023-10-02 15:24:14 +0200

    fix run_nfcore_rnaseq.sh

--- gpm/data/nfcore/run_nfcore_3mrnaseq.sh
+++ gpm/data/nfcore/run_nfcore_3mrnaseq.sh
@@ -1,21 +1,22 @@
 ################## test script #########################
-# nextflow run nf-core/rnaseq -profile test,docker
+# nextflow run nf-core/rnaseq -r 3.12.0 -profile test,docker --outdir results
 
 ################## GPM samplesheet #####################
-# gpm samplesheet -st 'forward' -sn true -si  samplesheet.csv FASTQ_DIR
+# gpm samplesheet -st 'auto' -sn true -si  samplesheet.csv FASTQ_DIR
 
-nextflow run nf-core/rnaseq -r 3.8.1 -profile docker \
-     --input samplesheet.csv --clip_r1 12 --outdir results \
-     # --genome gencode_GRCh38 \
-     # --genome gencode_GRCm39 \
-     --gencode --featurecounts_group_type gene_type \
-     --star_index false --save_reference \
-     #--additional_fasta /data/genomes/spikein/ERCC_ExFold_RNA_SpikeIn_Mixes/ERCC92.fa \
-     #--with_umi --umitools_extract_method "regex" --umitools_bc_pattern "^(?P<umi_1>.{6})(?P<discard_1>.{4}).*" 
+nextflow run nf-core/rnaseq -r 3.12.0 -profile docker -c nextflow.config \
+     --input samplesheet.csv --outdir results \
+     --genome GENCODE_GRCh38_v44 --gencode --featurecounts_group_type gene_type \
+     --max_cpus 45 \
+     --extra_salmon_quant_args="--noLengthCorrection" \
+     --extra_star_align_args="--alignIntronMax 1000000 --alignIntronMin 20 --alignMatesGapMax 1000000 --alignSJoverhangMin 8 --outFilterMismatchNmax 999 --outFilterMultimapNmax 20 --outFilterType BySJout --outFilterMismatchNoverLmax 0.1 --clip3pAdapterSeq AAAAAAAA" \
+     --with_umi --umitools_extract_method="regex" --umitools_bc_pattern="^(?P<umi_1>.{6})(?P<discard_1>.{4}).*"
 
-
-# Options for --genome:
-# gencode_GRCh38, gencode_GRCm39, hg38, mm10
-# Useful options: 
-# --removeRiboRNA
+# For rerun the pipeline, use: -resume
+# Options for genome: GENCODE_GRCh38_v44, GENCODE_GRCh38_v44_ERCC
+# QuantSeq 3’mRNA-Seq V2 Library Prep Kit with UDI:
+# --extra_star_align_args="--alignIntronMax 1000000 --alignIntronMin 20 --alignMatesGapMax 1000000 --alignSJoverhangMin 8 --outFilterMismatchNmax 999 --outFilterMultimapNmax 20 --outFilterType BySJout --outFilterMismatchNoverLmax 0.1 --clip3pAdapterSeq AAAAAAAA" \
+# --with_umi --umitools_extract_method="regex" --umitools_bc_pattern="^(?P<umi_1>.{6})(?P<discard_1>.{4}).*"
+# For any 3'mRNAseq: --extra_salmon_quant_args="--noLengthCorrection"
 # 
+# --gencode --featurecounts_group_type gene_type
