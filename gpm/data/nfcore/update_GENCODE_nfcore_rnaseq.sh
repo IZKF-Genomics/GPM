a5e8907cf36c03ebcf0821d5decead146b90eb69
Joseph Kuo <chao-chung.kuo@rwth-aachen.de>
2023-10-02 13:10:02 +0200

    add tee command for a log file.

--- gpm/data/nfcore/update_GENCODE_nfcore_rnaseq.sh
+++ gpm/data/nfcore/update_GENCODE_nfcore_rnaseq.sh
@@ -25,24 +25,25 @@ NAME=$5
 
 ERCC_FASTA="/data/genomes/spikein/ERCC_ExFold_RNA_SpikeIn_Mixes/ERCC92.fa"
 ERCC_GTF="/data/genomes/spikein/ERCC_ExFold_RNA_SpikeIn_Mixes/ERCC92.gtf"
+log_file="${FOLDER}/output.log"
 
 # Displaying the arguments
-echo "FASTA genome:      $FASTA_Genome"
-echo "FASTA transcripts: $FASTA_transcripts"
-echo "GTF annotation: $GTF"
-echo "Target Folder:  $FOLDER"
-echo "Genome name:    $NAME"
+echo "FASTA genome:      $FASTA_Genome" 2>&1 | tee -a "$log_file"
+echo "FASTA transcripts: $FASTA_transcripts" 2>&1 | tee -a "$log_file"
+echo "GTF annotation: $GTF" 2>&1 | tee -a "$log_file"
+echo "Target Folder:  $FOLDER" 2>&1 | tee -a "$log_file"
+echo "Genome name:    $NAME" 2>&1 | tee -a "$log_file"
 
 # Create a new folder
 mkdir -p $FOLDER
 
 # Run STAR index
 STAR_INDEX="${FOLDER}/STAR_index"
-STAR --runThreadN ${Num_cores} --runMode genomeGenerate --genomeDir $STAR_INDEX --genomeFastaFiles $FASTA_Genome
+STAR --runThreadN ${Num_cores} --runMode genomeGenerate --genomeDir $STAR_INDEX --genomeFastaFiles $FASTA_Genome 2>&1 | tee -a "$log_file"
 
 # Run Salmon index
 salmon_index="${FOLDER}/salmon_index"
-salmon index -t ${FASTA_transcripts} -i $salmon_index --gencode -p ${Num_cores}
+salmon index -t ${FASTA_transcripts} -i $salmon_index --gencode -p ${Num_cores} 2>&1 | tee -a "$log_file"
 
 # Combining ERCC sequences
 
@@ -52,14 +53,14 @@ cat $FASTA_transcripts $ERCC_FASTA > ${FOLDER}/${NAME}_ERCC_transcripts.fasta
 # GTF
 cat $GTF $ERCC_GTF > ${FOLDER}/${NAME}_ERCC.gtf
 # Transcripts
-gffread -w ${FOLDER}/${NAME}_ERCC_transcripts.gtf -g ${FOLDER}/${NAME}_ERCC.fasta ${FOLDER}/${NAME}_ERCC.gtf
+gffread -w ${FOLDER}/${NAME}_ERCC_transcripts.gtf -g ${FOLDER}/${NAME}_ERCC.fasta ${FOLDER}/${NAME}_ERCC.gtf 2>&1 | tee -a "$log_file"
 # Run STAR index
-STAR --runThreadN ${Num_cores} --runMode genomeGenerate --genomeDir ${STAR_INDEX}_ERCC --genomeFastaFiles ${FOLDER}/${NAME}_ERCC.fasta
+STAR --runThreadN ${Num_cores} --runMode genomeGenerate --genomeDir ${STAR_INDEX}_ERCC --genomeFastaFiles ${FOLDER}/${NAME}_ERCC.fasta 2>&1 | tee -a "$log_file"
 # Run Salmon index
-salmon index -t ${FOLDER}/${NAME}_ERCC_transcripts.fasta -i ${salmon_index}_ERCC --gencode -p ${Num_cores}
+salmon index -t ${FOLDER}/${NAME}_ERCC_transcripts.fasta -i ${salmon_index}_ERCC --gencode -p ${Num_cores} 2>&1 | tee -a "$log_file"
 
 # Export the config parameters for nextflow.config
-echo "Please add the following codes into nextflow.config:"
+echo "Please add the following codes into nextflow.config:" 2>&1 | tee -a "$log_file"
 echo "'${NAME}' {
       fasta  = '${FASTA_Genome}'
       gtf = '${GTF}'
@@ -67,7 +68,7 @@ echo "'${NAME}' {
       star = '${STAR_INDEX}'
       salmon = '${salmon_index}'
       gencode = true
-    }"
+    }" 2>&1 | tee -a "$log_file"
 
 echo "'${NAME}_ERCC' {
       fasta  = '${FOLDER}/${NAME}_ERCC.fasta'
@@ -76,4 +77,4 @@ echo "'${NAME}_ERCC' {
       star = '${STAR_INDEX}_ERCC'
       salmon = '${salmon_index}_ERCC'
       gencode = true
-    }"
+    }" 2>&1 | tee -a "$log_file"
