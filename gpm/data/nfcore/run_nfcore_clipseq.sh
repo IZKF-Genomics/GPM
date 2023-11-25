ee17b02a28c51c90769c3cf17be0ade940bcf8a6
Joseph Kuo <chao-chung.kuo@rwth-aachen.de>
2023-11-25 20:30:38 +0100

    update CLIPseq script

--- gpm/data/nfcore/run_nfcore_clipseq.sh
+++ gpm/data/nfcore/run_nfcore_clipseq.sh
@@ -1,7 +1,13 @@
 ################## test script ##################
 # nextflow run nf-core/clipseq -profile test, docker
 
-nextflow run nf-core/clipseq -profile docker \
-     --input samples.csv \
-     --genome gencode_hg38 --smrna_org mouse \
-     --peakcaller all --motif
+nextflow run goodwright/clipseq -latest -profile docker \
+     --samplesheet samples.csv --outdir results \
+     --genome GENCODE_GRCm38_v25 --fasta '/data/genomes/GRCm38/GRCm38.p6.genome.fa' \
+     --gtf '/data/genomes/GRCm38/gencode.vM25.annotation.gtf' \
+     --smrna_fasta Mus_musculus.smallRNA.fa.gz \
+     --peakcaller "icount,paraclu,pureclip,piranha" --skip_umi_dedupe true --motif
+
+# For rerun the pipeline, use: -resume
+# --smrna_fasta, search FASTQ files here:
+# https://github.com/nf-core/clipseq/tree/master/assets/small_rna
